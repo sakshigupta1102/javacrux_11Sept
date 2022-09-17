5659b62d5ae381debd078bea31c8efe2f53c80e0
sakshi gupta <sakshisg2000@gmail.com>
2022-09-17 19:04:51 +0530

    session 2

---
+++ src/session2/basics.java
@@ -0,0 +1,147 @@
+package session2;
+
+import java.util.Scanner;
+
+public class basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int num2 = scan.nextInt();
+		System.out.println("the input from user is " + n +" " +num2 );
+		
+		
+
+		// System.out.print("hello");
+		// System.out.println(" welcome to coding blocks ");
+		// System.out.println("hey");
+
+		// int a = 5;
+		// int b = 10;
+		// int c= a+b;
+		// System.out.println("the sum is " + c);
+		//
+		// int r = 10;
+		// int p=1000;
+		// int t=2;
+		// int si = (p*r*t)/100;
+		// System.out.println("the simple interest is " + si );
+
+//		int a = 9;
+//		int b = 9;
+		// a+= 5 ; // a = a+ 5;
+		// System.out.println(a+b);
+		// System.out.println(a-b);
+		// System.out.println(a*b);
+		// System.out.println(a/b);
+		// System.out.println(a%b);
+		//
+		// // System.out.println(a);
+		// System.out.println(a==b);
+		// System.out.println(a>b);
+		// System.out.println(a<b);
+		// System.out.println(a>=b);
+		// System.out.println(a!=b);
+		// System.out.println(a);
+		// a=90;
+		// b=100;
+		// System.out.println(!(a>100));
+		// int marks = 8+8;
+		// if(marks>90){
+		// System.out.println("you will get chocolates");
+		// }
+		// else
+		// System.out.println("you will fail ");
+		// }
+//		a = 10;
+//		if (a % 2 == 0) {
+//			System.out.println("even");
+//
+//		} else {
+//			System.out.println("odd");
+//		}
+//		int a= 10;
+//		int b= 8000;
+//		int c= 90;
+//		if( (a>b) && (a>c) ){
+//			System.out.println("greatest is" + a);
+//		}
+//		else if(b>c){
+//			System.out.println("greatest is" +b);
+//		}
+//		else{
+//			System.out.println("the greatest is" + c);
+//		}
+		
+//		int marks = 89;
+//		if(marks >= 90){
+//			System.out.println("A");
+//		}
+//		else if(marks < 90 && marks >= 80){
+//			System.out.println("B");
+//		}
+//		else if(marks < 80 && marks >= 70){
+//			System.out.println("C");
+//		}
+//		else if(marks < 70 && marks >= 60){
+//			System.out.println("D");
+//		}
+//		else if(marks < 60 && marks >= 50){
+//			System.out.println("E");
+//		}
+//		else if(marks < 50 && marks >= 40){
+//			System.out.println("F");
+//		}
+//		else if(marks < 40 && marks >= 0){
+//			System.out.println("fail");
+//		}
+//		else {
+//			System.out.println("wrong input ");
+//		}
+//		int num = 5 ;
+//		int i=num;
+//		int ans=1;
+//		while(i >= 1){
+//			ans = ans*i;
+//			i--;
+//		}
+//		System.out.println("fact is " + ans);
+//		
+////		
+//		
+//		 i = 1;
+//		int sum = 0 ;
+//		while(i<= 5){
+//			//System.out.println(sum);
+//			sum+= i;
+//			i= i+1;
+//			System.out.println(sum);
+//		}
+//		System.out.println(sum);
+//		System.out.println("loop variable value is" + i );
+//		
+//		int base = 2;
+//		int pow = 5;
+//		i=1;
+//		int ans=1;
+//		while(i<=pow){
+//		ans *= (base);
+//		i++;
+//		}
+//		System.out.println(ans);
+
+//		int
+
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
