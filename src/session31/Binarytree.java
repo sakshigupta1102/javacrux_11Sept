cb71026484f3659fa3a872d05cb98bce2082dca0
sakshi gupta <sakshisg2000@gmail.com>
2023-01-15 18:57:11 +0530

    binary tree class 2

--- src/session31/Binarytree.java
+++ src/session31/Binarytree.java
@@ -17,8 +17,14 @@ public class Binarytree {
 		}
 	}
 
-	Node root;
+	private Node root;
 	Scanner scan = new Scanner(System.in);
+
+	public Binarytree(String str) {
+
+		scan = new Scanner(str);
+		root = construct();
+	}
 
 	public Binarytree() {
 		this.root = construct();
@@ -35,6 +41,10 @@ public class Binarytree {
 		if (irc)
 			nn.right = construct();
 		return nn;
+	}
+
+	public void display() {
+		display(root);
 	}
 
 	public void display(Node node) {
@@ -56,6 +66,103 @@ public class Binarytree {
 		display(node.left);
 		display(node.right);
 
+	}
+
+	public int size() {
+		return size(root);
+	}
+
+	public int size(Node node) {
+
+		if (node == null)
+			return 0;
+
+		int lt = size(node.left);
+		int rt = size(node.right);
+		return lt + rt + 1;
+
+	}
+
+	public int max() {
+		return max(root);
+	}
+
+	public int max(Node node) {
+		if (node == null) {
+			return Integer.MIN_VALUE;
+		}
+		int lmax = max(node.left);
+		int rmax = max(node.right);
+		return Math.max(lmax, Math.max(rmax, node.data));
+
+	}
+
+	public boolean find(int item) {
+		return find(root, item);
+
+	}
+
+	public boolean find(Node node, int item) {
+		if (node == null)
+			return false;
+		if (item == node.data)
+			return true;
+		boolean lans = find(node.left, item);
+		boolean rans = find(node.right, item);
+		return lans || rans;
+
+	}
+
+	public int height() {
+		return height(root);
+	}
+
+	public int height(Node node) {
+		if (node == null)
+			return -1;
+		int lh = height(node.left);
+		int rh = height(node.right);
+		return Math.max(lh, rh) + 1;
+
+	}
+
+	public void preorder() {
+		preorder(root);
+	}
+
+	public void inorder() {
+		inorder(root);
+	}
+
+	public void postorder() {
+		postorder(root);
+	}
+
+	public void preorder(Node node) {
+		// print left right
+		if (node == null)
+			return;
+		System.out.print(node.data + " ");
+		preorder(node.left);
+		preorder(node.right);
+	}
+
+	public void inorder(Node node) {
+		if (node == null) {
+			return;
+		}
+		inorder(node.left);
+		System.out.print(node.data + " ");
+		inorder(node.right);
+	}
+
+	public void postorder(Node node) {
+		if (node == null) {
+			return;
+		}
+		postorder(node.left);
+		postorder(node.right);
+		System.out.print(node.data + " ");
 	}
 
 }
--- src/session31/btclient.java
+++ src/session31/btclient.java
@@ -3,8 +3,10 @@ package session31;
 public class btclient {
 
 	public static void main(String[] args) {
-	Binarytree tree = new  Binarytree();
-	tree.display(tree.root);
+		String str = "10 true 20 true 40 false false true 50 false false true 30 false true 60 true 70 false false false";
+		Binarytree tree = new Binarytree(str);
+		tree.display();
+		tree.postorder();
 
 	}
 
