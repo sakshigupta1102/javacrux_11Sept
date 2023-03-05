a6f8ed0ba41c62179ec33068aef6e5fc5c634ce0
sakshi gupta <sakshisg2000@gmail.com>
2023-03-05 10:22:33 +0530

    .

--- src/session9/bubblesort.java
+++ src/session9/bubblesort.java
@@ -15,10 +15,10 @@ public class bubblesort {
 				}
 			}
 		}
-for(int i=0;i<arr.length;i++){
-	System.out.print(arr[i]+" ");
-}
-		
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+
 	}
 
 }
--- src/session9/insertionsort.java
+++ src/session9/insertionsort.java
@@ -1,21 +1,19 @@
 package session9;
 
 public class insertionsort {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr = {5,4,8,1,1};
-		for(int i=1;i<arr.length;i++){
+		int[] arr = { 5, 4, 8, 1, 1 };
+		for (int i = 1; i < arr.length; i++) {
 			int temp = arr[i];
-			int j=i-1;
-			while(j>=0 && temp < arr[j]){
-				arr[j+1]= arr[j];
-				j--;
+			int j = i - 1;
+			for (j = i - 1; j >= 0 && temp < arr[j]; j--) {
+				arr[j + 1] = arr[j];
 			}
-			arr[j+1] = temp;
+			arr[j + 1] = temp;
 		}
-		for(int i=0;i<arr.length;i++){
-			System.out.print(arr[i]+" ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
 		}
 
 	}
